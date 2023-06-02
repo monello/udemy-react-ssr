7c460c89c3e64ff544dfc62f1217f8ffb74d58fc
Morne Louw <louw.morne@gmail.com>
2023-06-02 07:59:18 +0200

    using ReactDom.renderToString to render a react component from express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 
+// imports to be able to render a React component
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
 // create a new express app
 const app = express();
 
 // create a root route
 app.get('/', (req, res) => {
-    res.send("hello world");
+    // first we need to convert the React component to an HTML string.
+    // ... this is where we use "renderToString()" instead of "render()" as we do in normal React apps
+    const content = renderToString(<Home />);
+    // We still send an HTML string back, but this time it will be the pre-rendered React component
+    res.send(content);
 });
 
 // tell our express app to listen on port 3000
