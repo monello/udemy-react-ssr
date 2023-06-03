d87fb85bf82043be70b3c497f3b1fccc19360f08
Morne Louw <louw.morne@gmail.com>
2023-06-03 09:40:14 +0200

    refactor: reduce server bundle size and processing time

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
     // Inform Webpack that we're building a bundle for NodeJS, rather than for the browser
@@ -15,7 +16,8 @@ const config = {
         // we don't have to manyally go create the 'build' folder webpack will create it if it doesn't exist
         // it will also recreate it each time we run the build script (if it doesn't exist)
         path: path.resolve(__dirname, 'build')
-    }
+    },
+    externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
