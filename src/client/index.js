e8bbc5439fd60071d15b5aacf969e26e3f4e0939
Morne Louw <louw.morne@gmail.com>
2023-06-03 14:42:37 +0200

    routing: routes config and client-side routes

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
 // Entry point file for the client-side application
 import React from "react";
 import ReactDOM from 'react-dom';
-import Home from "./components/Home";
+import { BrowserRouter } from "react-router-dom";
+import Routes from "./Routes";
 
 const rootElem = document.querySelector('#root');
-ReactDOM.hydrate(<Home />, rootElem);
+ReactDOM.hydrate(
+    <BrowserRouter><Routes /></BrowserRouter>,
+    rootElem
+);
