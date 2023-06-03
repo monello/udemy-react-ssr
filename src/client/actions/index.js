b92769daa882b9cd463482a9eb1fba3aca22c0e7
Morne Louw <louw.morne@gmail.com>
2023-06-03 21:13:04 +0200

    readux: the fetchUsers action creator

---
+++ src/client/actions/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+// TYPES
+export const FETCH_USERS = 'fetch_users';
+
+// ACTION CREATORS
+export const fetchUsers = () => async dispatch => {
+    const res = await axios.get('https://react-ssr-api.herokuapp.com/users');
+
+    dispatch({
+        action: FETCH_USERS,
+        payload: res
+    });
+};
