69caa323991cc83b2c9ce94ecfda9146841528c9
Morne Louw <louw.morne@gmail.com>
2023-06-04 09:59:47 +0200

    routing: using react-router-config package

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Route } from 'react-router-dom';
 import Home from "./components/Home";
 import UsersList from "./components/UsersList";
 
-export default () => {
-    return (
-        <div>
-            <Route exact path="/" component={Home} />
-            <Route path="/users" component={UsersList} />
-        </div>
-    );
-};
+// This is how you handle Routing when using the `react-router-config` package
+export default [
+    {
+        path: '/',
+        exact: true,
+        component: Home
+    },
+    {
+        path: '/users',
+        component: UsersList
+    }
+];
