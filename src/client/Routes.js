d0c00be446e323ac65076bf3f8a7966a94a65886
Morne Louw <louw.morne@gmail.com>
2023-06-09 16:14:15 +0200

    refactor: create a root-level App component and nested routing

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,16 +1,24 @@
 import React from "react";
+import App from "./App";
 import HomePage from "./pages/HomePage";
 import UsersListPage, { loadData } from "./pages/UsersListPage";
 
 // This is how you handle Routing when using the `react-router-config` package
 export default [
     {
-        ...HomePage,
-        path: '/',
-        exact: true,
-    },
-    {
-        ...UsersListPage,
-        path: '/users',
+        ...App,
+        // nested routes
+        routes: [
+            {
+                ...HomePage,
+                path: '/',
+                exact: true,
+            },
+            {
+                ...UsersListPage,
+                path: '/users',
+            }
+        ]
     }
 ];
+
