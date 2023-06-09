412cc145dada85b8c73b0a836bf39571af122c71
Morne Louw <louw.morne@gmail.com>
2023-06-09 17:06:19 +0200

    auth: add the header navigation buttons

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -4,9 +4,23 @@ import { connect } from 'react-redux';
 
 const Header = ({ auth }) => {
     console.log('My Auht status is: ', auth);
+
+    // We use normal anchor elements here because we want normal HTML link behaviour we are not navigating inside our app
+    // When we want to implement naviagtion within our app we use the Link component (see below)
+    const autButton = auth ? (
+        <a href="/api/logout">Logout</a>
+    ) : (
+        <a href="/api/auth/google">Login</a>
+    );
+
     return (
         <div>
             <Link to="/">React SSR</Link>
+            <div>
+                <Link to="/users">Users</Link>
+                <Link to="/admins">Admins</Link>
+                {autButton}
+            </div>
         </div>
     );
 };
