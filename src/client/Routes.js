83b3dfef594823ab8059ba2e35795be88ff4c804
Morne Louw <louw.morne@gmail.com>
2023-06-10 07:05:22 +0200

    errors: add page-not-found page and route

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./App";
 import HomePage from "./pages/HomePage";
 import UsersListPage, { loadData } from "./pages/UsersListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 // This is how you handle Routing when using the `react-router-config` package
 export default [
@@ -17,6 +18,9 @@ export default [
             {
                 ...UsersListPage,
                 path: '/users',
+            },
+            {
+                ...NotFoundPage
             }
         ]
     }
